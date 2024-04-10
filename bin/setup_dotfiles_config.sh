06c354520c4da496ad5e73350cbfd1947ff218a9
Florian Hild <florian.hild@gmail.com>
2024-04-10 18:18:49 +0200

    feature: improve setup_dotfiles_config.sh :sparkles:

---
+++ bin/setup_dotfiles_config.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+export LANG=C
+declare -r __SCRIPT_VERSION__='1.0'
+declare -r DOT_PATH="${HOME}/.dotfiles"
+
+if ! grep -wq 'dotfiles/\.bash_profile' ~/.bash_profile; then
+echo "Add to ${HOME}/.bash_profile"
+cat >> ${HOME}/.bash_profile <<EOF
+
+# Load bash_profile from dotfiles
+if [[ -f ${DOT_PATH}/.bash_profile ]]; then
+  . ${DOT_PATH}/.bash_profile
+fi
+
+EOF
+fi
+
+if ! grep -wq 'dotfiles/\.bashrc' ~/.bashrc; then
+echo "Add to ${HOME}/.bashrc"
+cat >> ${HOME}/.bashrc <<EOF
+
+# Load bashrc from dotfiles
+if [[ -f ${DOT_PATH}/.bashrc ]]; then
+  . ${DOT_PATH}/.bashrc
+fi
+
+EOF
+fi
+
+if ! grep -wq 'dotfiles/\.gitconfig' ~/.gitconfig; then
+echo "Add to ${HOME}/.gitconfig"
+cat >> ${HOME}/.gitconfig <<EOF
+
+[include]
+  path = ${DOT_PATH}/.gitconfig
+
+EOF
+fi
+
+echo "Setup symlinks"
+ln -sfn ${DOT_PATH}/.vim ${HOME}/.vim
+ln -sfn ${DOT_PATH}/.vimrc ${HOME}/.vimrc
+ln -sfn ${DOT_PATH}/.digrc ${HOME}/.digrc
+ln -sfn ${DOT_PATH}/.tmux.conf ${HOME}/.tmux.conf
+ln -sfn ${DOT_PATH}/.tmux.conf.local ${HOME}/.tmux.conf.local
