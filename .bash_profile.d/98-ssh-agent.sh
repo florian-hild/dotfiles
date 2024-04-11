cba4478ef234b2af6ab17aae15c0002ee48c6f4b
Florian Hild <florian.hild@sup-logistik.de>
2024-04-11 17:16:03 +0200

    feature: make ssh agent configurable :sparkles:

--- .bash_profile.d/98-ssh-agent.sh
+++ .bash_profile.d/98-ssh-agent.sh
@@ -1,21 +1,25 @@
-set_ssh_agent() {
-  if pgrep -f ssh-agent; then
-    export SSH_AUTH_SOCK=${HOME}/.ssh/ssh_auth_sock
-    export SSH_AGENT_PID=$(pgrep ssh-agent)
-  else
-    rm -f ${HOME}/.ssh/ssh_auth_sock
-    eval $(ssh-agent -a ${HOME}/.ssh/ssh_auth_sock)
+# SSH Agent
 
-    if [[ -r ${HOME}/.ssh/id_ed25519 ]]; then
-      ssh-add ${HOME}/.ssh/id_ed25519
-    elif [[ -r ${HOME}/.ssh/id_rsa ]]; then
-      ssh-add ${HOME}/.ssh/id_rsa
-    elif [[ -r ${HOME}/.ssh/ssh_key ]]; then
-      ssh-add ${HOME}/.ssh/ssh_key
+if [[ -n "${USE_SSH_AGENT// }" ]]; then
+  set_ssh_agent() {
+    if pgrep -f ssh-agent; then
+      export SSH_AUTH_SOCK=${HOME}/.ssh/ssh_auth_sock
+      export SSH_AGENT_PID=$(pgrep ssh-agent)
     else
-      echo "ssh-agent: No SSH key found"
-    fi
-  fi
-}
+      rm -f ${HOME}/.ssh/ssh_auth_sock
+      eval $(ssh-agent -a ${HOME}/.ssh/ssh_auth_sock)
 
-set_ssh_agent
+      if [[ -r ${HOME}/.ssh/id_ed25519 ]]; then
+        ssh-add ${HOME}/.ssh/id_ed25519
+      elif [[ -r ${HOME}/.ssh/id_rsa ]]; then
+        ssh-add ${HOME}/.ssh/id_rsa
+      elif [[ -r ${HOME}/.ssh/ssh_key ]]; then
+        ssh-add ${HOME}/.ssh/ssh_key
+      else
+        echo "ssh-agent: No SSH key found"
+      fi
+    fi
+  }
+
+  set_ssh_agent
+fi
