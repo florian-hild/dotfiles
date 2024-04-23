af5baa17683592bc32dda0d9db638304ec06bc74
Florian Hild <florian.hild@sup-logistik.de>
2024-04-23 15:44:10 +0200

    add: sql script show_table_stats.sql :rocket:

---
+++ oracle/sql/dba/show_table_stats.sql
@@ -0,0 +1,17 @@
+set pagesize 2000
+set linesize 250
+alter session set nls_date_format='DD-MON-YYYY HH24:MI:SS';
+
+col OWNER format a25;
+col TABLE_NAME format a30
+col OBJECT_TYPE format a15
+col LAST_ANALYZED format a20
+col STATTYPE_LOCKED format a5
+col STALE_STATS format a5
+
+SELECT OWNER,TABLE_NAME,OBJECT_TYPE,LAST_ANALYZED,STATTYPE_LOCKED,STALE_STATS
+  FROM dba_tab_statistics
+  WHERE STATTYPE_LOCKED!=' '
+  AND OWNER not in ('SYS','WMSYS','GSMADMIN_INTERNAL')
+  ORDER BY STALE_STATS,TABLE_NAME;
+
