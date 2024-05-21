b79e88604339adf40d37598e78b25875ebec4dd4
Florian Hild <florian.hild@sup-logistik.de>
2024-05-21 16:26:00 +0200

    add: sql script remote_objects.sql :rocket:

---
+++ oracle/sql/dba/remote_objects.sql
@@ -0,0 +1,14 @@
+set pagesize 2000
+set linesize 250
+
+col OWNER format a10
+col SYNONYM_NAME  format a45
+col TABLE_OWNER   format a20
+col TABLE_NAME    format a45
+col DB_LINK       format a50
+col ORIGIN_CON_ID format 9999
+
+SELECT OWNER,SYNONYM_NAME,TABLE_OWNER,TABLE_NAME,DB_LINK,ORIGIN_CON_ID
+  FROM dba_synonyms
+  WHERE DB_LINK != ' '
+  ORDER BY TABLE_OWNER,SYNONYM_NAME,TABLE_NAME ASC;
