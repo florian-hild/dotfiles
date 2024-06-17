7f4b91fcd5ad38d04ca606e7c66c93d5bc8aaca3
Florian Hild <florian.hild@sup-logistik.de>
2024-06-17 09:40:59 +0200

    fix: column width in table_size.sql :adhesive_bandage:

--- oracle/sql/dba/table_size.sql
+++ oracle/sql/dba/table_size.sql
@@ -11,7 +11,7 @@ alter session set nls_date_format='DD-MON-YYYY HH24:MI:SS';
 col OWNER format a20
 col OBJECT_NAME format a30
 col TABLESPACE_NAME format a20
-col SEGMENT_TYPE format a10
+col SEGMENT_TYPE format a15
 
 SELECT OWNER,SEGMENT_NAME,SEGMENT_TYPE,TABLESPACE_NAME,BYTES / 1024 / 1024 "Size in MB",EXTENTS
   FROM dba_segments
