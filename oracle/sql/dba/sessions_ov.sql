820e55c58e3df865310df2de19aec80cb156f53b
Florian Hild <florian.hild@sup-logistik.de>
2024-11-22 10:52:12 +0100

    fix: formating in sessions_ov.sql :adhesive_bandage:

--- oracle/sql/dba/sessions_ov.sql
+++ oracle/sql/dba/sessions_ov.sql
@@ -6,19 +6,12 @@
 
 set pagesize 2000
 set linesize 250
-alter session set nls_date_format='DD-MON-YYYY HH24:MI:SS';
+set wrap off
 
-col SID format 99999
-col SERIAL# format 999999
-col PROCESS format a15
-col USERNAME format a16
-col STATUS format a8
-col OSUSER format a15
+col CLIENT_IDENTIFIER format a25
 col PROGRAM format a60
-col CLIENT_IDENTIFIER format a17
-col LOGON_TIME format a20
-col IP format a15
-col MACHINE format a30
+col USERNAME format a16
+col MACHINE format a35
 
 SELECT count(*) ctn,CLIENT_IDENTIFIER,PROGRAM,USERNAME,MACHINE
   FROM v$session
