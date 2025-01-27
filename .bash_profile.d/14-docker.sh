c45e468a12241d32fe59d669665748454cc553d1
Florian Hild <florian.hild@sup-logistik.de>
2025-01-27 14:57:54 +0100

    feat: add docker alias

--- .bash_profile.d/14-docker.sh
+++ .bash_profile.d/14-docker.sh
@@ -2,6 +2,7 @@
 
 if command -v docker > /dev/null 2>&1; then
   alias dps='docker ps --all --format "table {{.Names}}\t{{.Image}}\t{{.Status}}"'
+  alias dimages='docker images --tree'
   alias dshell='docker exec --interactive --tty'
   alias dive="docker run -ti --rm  -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive"
 fi
