2e05716a9941cdfe4087989b0df915e0c30f9a92
Florian Hild <florian.hild@sup-logistik.de>
2025-01-27 14:40:07 +0100

    feat: improve download_rhel_iso.sh

--- src/bash/download_rhel_iso.sh
+++ src/bash/download_rhel_iso.sh
@@ -7,32 +7,114 @@
 #-------------------------------------------------------------------------------
 
 export LANG=C
-declare -r __SCRIPT_VERSION__='1.0'
+declare -r __SCRIPT_VERSION__='2.0'
 
-read -p "Enter Red Hat API token: " offline_token
+
+# help(exit_code)
+# Print help message and exit
+help() {
+  local -r exit_code="${1:-0}"
+  # Print help text
+  cat << EOF
+Usage:
+  ${0} [options]
+
+Options:
+  --checksum-sha256 <sha256>     Set SHA-265 checksum from Red Hat ISO image (required)
+  -h, --help                     Display this help and exit
+  -v, --verbose                  Print debugging messages
+  -V, --version                  Display version and exit
+
+Examples:
+  Download RedHat ISO
+  Get Red Hat offline API token from https://access.redhat.com/management/api
+  \$ ${0} --checksum-sha256 0bb7600c3187e89cebecfcfc73947eb48b539252ece8aab3fe04d010e8644ea9
+
+EOF
+  exit ${exit_code}
+}
+
+
+
+# main()
+# Start of script
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  if [[ ${#} -eq "0" ]] || [[ ${@} == "-" ]] || [[ ${@} == "--" ]]; then
+    echo "Syntax or usage error (1)" >&2
+    echo
+    help 128
+  fi
+
+  OPTS="$(getopt -o 'hvV' --long 'checksum-sha256:,help,verbose,version' -n "${0}" -- "${@}")"
+  if [[ "${?}" != "0" ]] ; then
+    echo "Syntax or usage error (2)" >&2
+    echo
+    help 128
+  fi
+  eval set -- "$OPTS"
+
+  while true; do
+    case "${1}" in
+    --checksum-sha256)
+      declare -r checksum_sha256="${2}"
+      shift 2
+      ;;
+    -h | --help)
+      help 0
+      ;;
+    -v | --verbose)
+      declare -r verbose="1"
+      set -xv  # Set xtrace and verbose mode.
+      shift
+      ;;
+    -V | --version)
+      echo "${__SCRIPT_VERSION__}"
+      exit 0
+      ;;
+    -- )
+      shift
+      break
+      ;;
+    *)
+      echo "Syntax or usage error (3)" >&2
+      echo
+      help 128
+      ;;
+    esac
+  done
+fi
+
+if [[ -z "${checksum_sha256// }" ]]; then
+  echo "Error: You must specify a SHA-265 checksum from the ISO image."
+  echo
+  help 128
+fi
+
+echo "You can get a new offline API token from this url: https://access.redhat.com/management/api"
+read -p "Enter Red Hat offline API token: " offline_token
 if [[ -z "${offline_token// }" ]]; then
   echo "Error: Variable \"offline_token\" not set."
   exit 1
 fi
 
-read -p "Enter Red Hat downloads SHA-256 checksum: " checksum
-if [[ -z "${checksum// }" ]]; then
-  echo "Error: Variable \"checksum\" not set."
-  exit 1
-fi
-
 # get an access token
-access_token=$(curl https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token -d grant_type=refresh_token -d client_id=rhsm-api -d refresh_token=$offline_token | jq -r '.access_token')
+access_token=$(curl --silent \
+                    --data grant_type=refresh_token \
+                    --data client_id=rhsm-api \
+                    --data refresh_token=${offline_token} \
+                    https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token | jq -r '.access_token')
 
 # get the filename and download url
-image=$(curl -H "Authorization: Bearer $access_token" "https://api.access.redhat.com/management/v1/images/${checksum}/download")
+image=$(curl --silent \
+             --header "Authorization: Bearer ${access_token}" \
+             "https://api.access.redhat.com/management/v1/images/${checksum_sha256}/download")
 filename=$(echo ${image} | jq -r .body.filename)
+[[ -z "${filename// }" ]] && echo -e "Error: Could not get access token. Please check your API Token.\n${image}\n"
 url=$(echo ${image} | jq -r .body.href)
 
 # download the file
-set -x
-curl ${url} --output ${filename}
-set +x
+echo "Downloading ${filename} ..."
+curl --output ${filename} ${url}
 
 exit
 
