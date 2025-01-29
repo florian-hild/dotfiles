01e8e141d5b085f6951177072f396ca55e0d33dd
Florian Hild <florian.hild@sup-logistik.de>
2025-01-29 15:42:54 +0100

    feat: add support for pyenv

--- .bash_profile.d/11-ansible.sh
+++ .bash_profile.d/11-ansible.sh
@@ -1,6 +1,9 @@
 # Ansible
 
 if [[ -n "${ANSIBLE// }" ]]; then
+  export ANSIBLE_HOME="${HOME}/.ansible"
+  export ANSIBLE_CONFIG="${ANSIBLE}/ansible.cfg"
+  export ANSIBLE_FORCE_COLOR=True
   export INVENTORIES="${ANSIBLE}/environments"
   export CUST="${INVENTORIES}/customers"
   export INT="${INVENTORIES}/intern"
@@ -9,59 +12,64 @@ if [[ -n "${ANSIBLE// }" ]]; then
   export ROLES="${ANSIBLE}/roles"
   export ROLESWIP="${ANSIBLE}/roles_wip"
 
-function ansible-container(){
-    local ansible_args="${@}"
-    docker run \
-    --rm \
-    --init \
-    --volume ~/.ssh/${ANSIBLE_DEFAULT_SSH_KEY-"id_ed25519"}:/home/ansible/.ssh/${ANSIBLE_DEFAULT_SSH_KEY-"id_ed25519"}:ro \
-    --volume ~/project/ansible:/ansible:ro \
-    --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
-    --name ansible-worker-$(date +'%Y%m%d_%H%M%S') \
-    local/ansible:${ANSIBLE_DEFAULT_VERSION-"latest"}-alpine \
-    bash -c "${ansible_args}"
-}
+  if [[ -n "${ANSIBLE_RUN_ENV// }" ]]; then
+    if [[ "${ANSIBLE_RUN_ENV// }" == "container" ]]; then
+      function ansible-container(){
+        local ansible_args="${@}"
+        docker run \
+        --rm \
+        --init \
+        --volume ~/.ssh/${ANSIBLE_DEFAULT_SSH_KEY-"id_ed25519"}:/home/ansible/.ssh/${ANSIBLE_DEFAULT_SSH_KEY-"id_ed25519"}:ro \
+        --volume ${ANSIBLE}:/home/ansible/ansible:ro \
+        --volume ${ANSIBLE_HOME}:/home/ansible/.ansible:rw \
+        --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
+        --env WORK_PATH="/home/ansible/ansible/${PWD#*ansible/}" \
+        --name ansible-worker-$(date +'%Y%m%d_%H%M%S') \
+        local/ansible:${ANSIBLE_DEFAULT_VERSION-"latest"}-alpine \
+        bash -c "${ansible_args}"
+      }
 
-function ansible-playbook(){
-    local ansible_playbook_args="${@}"
-    docker run \
-    --rm \
-    --init \
-    --volume ~/.ssh/${ANSIBLE_DEFAULT_SSH_KEY-"id_ed25519"}:/home/ansible/.ssh/${ANSIBLE_DEFAULT_SSH_KEY-"id_ed25519"}:ro \
-    --volume ~/project/ansible:/ansible:ro \
-    --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
-    --env WORK_PATH="/ansible/${PWD#*ansible/}" \
-    --name ansible-worker-$(date +'%Y%m%d_%H%M%S') \
-    local/ansible:${ANSIBLE_DEFAULT_VERSION-"latest"}-alpine \
-    bash -c "ansible-playbook \${WORK_PATH}/${ansible_playbook_args}"
-}
+      function ansible-playbook(){
+        local ansible_playbook_args="${@}"
+        ansible-container "ansible-playbook \${WORK_PATH}/${ansible_playbook_args}"
+      }
 
-function ansible-lint(){
-    local lint_target=${1:-"/ansible/${PWD#*ansible/}/*"}
-    local work_path_suffix=${PWD#*ansible/}
+      function ansible-lint(){
+        local lint_target=${1:-"/home/ansible/ansible/${PWD#*ansible/}/*"}
+        local work_path_suffix=${PWD#*ansible/}
 
-    echo "Lint target: ${lint_target}"
-    echo "Work path suffix: ${work_path_suffix}"
-    echo "Linting using yamllint..."
-    docker run \
-      --rm \
-      --init \
-      --volume ~/project/ansible:/ansible:ro \
-      --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
-      --env LINT_CONFIG_FILE="/ansible/${work_path_suffix%%/*}/.yamllint" \
-      --name ansible-worker-$(date +'%Y%m%d_%H%M%S') \
-      local/ansible:${ANSIBLE_DEFAULT_VERSION:-"latest"}-alpine \
-      bash -c "find ${lint_target} -type f \( -name '*.yaml' -o -name '*.yml' \) -exec yamllint -c \${LINT_CONFIG_FILE} {} +"
+        echo "Lint target: ${lint_target}"
+        echo "Work path suffix: ${work_path_suffix}"
+        echo "Linting using yamllint..."
+        docker run \
+        --rm \
+        --init \
+        --volume ${ANSIBLE}:/home/ansible/ansible:ro \
+        --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
+        --env LINT_CONFIG_FILE="/home/ansible/ansible/${work_path_suffix%%/*}/.yamllint" \
+        --name ansible-worker-$(date +'%Y%m%d_%H%M%S') \
+        local/ansible:${ANSIBLE_DEFAULT_VERSION:-"latest"}-alpine \
+        bash -c "find ${lint_target} -type f \( -name '*.yaml' -o -name '*.yml' \) -exec yamllint -c \${LINT_CONFIG_FILE} {} +"
 
-    echo "Linting using ansible-lint..."
-    docker run \
-      --rm \
-      --init \
-      --volume ~/project/ansible:/ansible:ro \
-      --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
-      --env LINT_CONFIG_FILE="/ansible/${work_path_suffix%%/*}/.ansible-lint" \
-      --name ansible-worker-$(date +'%Y%m%d_%H%M%S') \
-      local/ansible:${ANSIBLE_DEFAULT_VERSION:-"latest"}-alpine \
-      bash -c "ansible-lint --format brief --config-file \${LINT_CONFIG_FILE} ${lint_target}"
-}
+        echo "Linting using ansible-lint..."
+        docker run \
+        --rm \
+        --init \
+        --volume ${ANSIBLE}:/home/ansible/ansible:ro \
+        --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
+        --env LINT_CONFIG_FILE="/home/ansible/ansible/${work_path_suffix%%/*}/.ansible-lint" \
+        --name ansible-worker-$(date +'%Y%m%d_%H%M%S') \
+        local/ansible:${ANSIBLE_DEFAULT_VERSION:-"latest"}-alpine \
+        bash -c "ansible-lint --format brief --config-file \${LINT_CONFIG_FILE} ${lint_target}"
+      }
+    elif [[ "${ANSIBLE_RUN_ENV// }" == "venv" ]]; then
+      if [[ -n "${ANSIBLE_RUN_VENV_PATH// }" ]]; then
+        source ${ANSIBLE_RUN_VENV_PATH// }/bin/activate
+      else
+        echo "Error: variable \$ANSIBLE_RUN_VENV_PATH not set."
+      fi
+    fi
+  else
+    echo "Error: variable \$ANSIBLE_RUN_ENV not set."
+  fi
 fi
