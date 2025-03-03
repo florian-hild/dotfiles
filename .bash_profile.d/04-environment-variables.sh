28912234ad39e53eb58fcac0593f1257e0c667e1
Florian Hild <florian.hild@sup-logistik.de>
2025-03-03 13:37:06 +0100

    feat(bash_profile): set env var only if directory exists

--- .bash_profile.d/04-environment-variables.sh
+++ .bash_profile.d/04-environment-variables.sh
@@ -5,5 +5,5 @@ export GPG_TTY=$(tty) # Needed to use gpg signed commits with git
 export EDITOR=vim
 declare -x -r DOT="$(dirname $(dirname "${BASH_SOURCE[0]}"))"
 export BIN="$(dirname $(dirname "${BASH_SOURCE[0]}"))/bin"
-export GIT="${HOME}/git"
-export WORKSPACE="${HOME}/workspace"
+[[ -d ${HOME}/git ]] && export GIT="${HOME}/git"
+[[ -d ${HOME}/workspace ]] && export WORKSPACE="${HOME}/workspace"
