47cecbe52572847222c3c2a98094e01c92b31731
Florian Hild <florian.hild@sup-logistik.de>
2025-03-03 13:54:14 +0100

    fix(bash_profile): bash completion

--- .bash_profile.d/10-bash-completion.sh
+++ .bash_profile.d/10-bash-completion.sh
@@ -1,13 +1,23 @@
 # bash-completion
 
 # Add all ssh commands from history
-[[ -n "${BASH_PROFILE_DEBUG}" ]] && print_debug_msg "${BASH_SOURCE[0]}" "Load bash completion from HISTFILE ssh commands"
+complete -W "$(command grep -Eo "^ssh[[:space:]]+([\.0-9a-zA-Z]+@)?([a-zA-Z0-9.-]+)" "${HISTFILE}" | LANG=C sort | uniq)" ssh
 
-# Mac OS
+# Mac OS Brew
 if [[ "$(uname -s)" == "Darwin" ]]; then
-  [[ -n "${BASH_PROFILE_DEBUG}" ]] && print_debug_msg "${BASH_SOURCE[0]}" "Load bash completion from brew"
   export BASH_COMPLETION_COMPAT_DIR="$(brew --prefix)/etc/bash_completion.d"
-  [[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+  if [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]]; then
+    source "$(brew --prefix)/etc/profile.d/bash_completion.sh"
+  else
+    for completion in "$(brew --prefix)/etc/bash_completion.d/*"; do
+      [[ -r "${completion}" ]] && source "${completion}"
+    done
+    unset completion
+  fi
 fi
 
-complete -W "$(command grep -Eo "^ssh[[:space:]]+([\.0-9a-zA-Z]+@)?([a-zA-Z0-9.-]+)" "${HISTFILE}" | LANG=C sort | uniq)" ssh
+
+# OpenTofu
+if command -v tofu > /dev/null 2>&1; then
+  complete -C "$(which tofu)" tofu
+fi
