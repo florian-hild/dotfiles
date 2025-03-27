488a86381b6fffec82dad0981ccd9999d20c4c36
Florian Hild <florian.hild@sup-logistik.de>
2025-03-27 16:57:13 +0100

    feat: update sql script

--- oracle/sql/dba/kill_dnf_sessions.sql
+++ oracle/sql/dba/kill_dnf_sessions.sql
@@ -21,6 +21,19 @@ col spoolname new_value spoolname
 SELECT '$HOME/kill_dnf_sessions_' || NVL(UPPER('&dstuser'), 'ALL') || '_' || to_char(sysdate, 'yyyy-mm-dd_hh24-mi-ss') || '.sql' spoolname FROM dual;
 SPOOL '&spoolname'
 
+-- Save output to file
+col USERNAME format a30
+col DNF_count format 9999
+col CNF_count format 9999
+
+SELECT '-- USERNAME                       DNF_COUNT CNF_COUNT' FROM dual;
+SELECT '-- ------------------------------ --------- ---------' FROM dual;
+SELECT '-- ' || USERNAME AS USERNAME, COUNT(CASE WHEN CLIENT_IDENTIFIER LIKE 'DNF%' THEN 1 END) AS DNF_count, COUNT(CASE WHEN CLIENT_IDENTIFIER LIKE 'CNF%' THEN 1 END) AS CNF_count
+  FROM v$session
+  WHERE CLIENT_IDENTIFIER LIKE 'DNF%' OR CLIENT_IDENTIFIER LIKE 'CNF%'
+  GROUP BY USERNAME
+  ORDER BY DNF_count;
+
 select 'alter system kill session ''' || SID || ',' || SERIAL# || ''';'
   from v$session
   where CLIENT_IDENTIFIER like 'DNF%'
