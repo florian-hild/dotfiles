2be8ac104ce317d5eec8069d3c723546f6943892
Florian Hild <florian.hild@sup-logistik.de>
2025-03-31 21:42:46 +0200

    fix: order $PATH

--- .bash_profile.d/01-path.sh
+++ .bash_profile.d/01-path.sh
@@ -1,31 +1,56 @@
 # Set PATH
 
 function setPath() {
-  local lpath=${1}
+  local lpath="${1}"
   if [[ ! "${PATH}" =~ "${lpath}" ]] && [[ -d ${lpath} ]]; then
     [[ -n "${BASH_PROFILE_DEBUG}" ]] && print_debug_msg "${BASH_SOURCE[0]}" "Add to PATH ${lpath}"
     export PATH="${lpath}:${PATH}"
+  else
+    [[ -n "${BASH_PROFILE_DEBUG}" ]] && print_debug_msg "${BASH_SOURCE[0]}" "Skip adding to PATH ${lpath}"
+  fi
+
+  if [[ ! "${CUSTOM_PATH}" =~ "${lpath}" ]] && [[ -d ${lpath} ]]; then
+    export CUSTOM_PATH="${lpath}:${CUSTOM_PATH}"
   fi
 }
 
+function reorderPath() {
+  [[ -n "${BASH_PROFILE_DEBUG}" ]] && print_debug_msg "${BASH_SOURCE[0]}" "Run reorderPath()"
+  local original_path="${PATH}"
+  local new_path=""
+  local IFS=":"
+  local seen=()
+  declare -A path_map
 
-# UNIX paths
-linux_paths=(
-  "/usr/local/bin"
-)
-
-if [[ -n "${linux_paths}" ]]; then
-  for lpath in "${linux_paths[@]}"; do
-    setPath "${lpath}"
+  # Add CUSTOM_PATH entries first, ensuring uniqueness
+  for dir in ${CUSTOM_PATH}; do
+      if [[ -d "${dir}" && -z "${path_map[${dir}]}" ]]; then
+          new_path+="${dir}:"
+          path_map[${dir}]=1
+      fi
   done
 
-  unset linux_paths
-fi
+  # Add remaining PATH entries while avoiding duplicates
+  for dir in ${original_path}; do
+      if [[ -d "${dir}" && -z "${path_map[${dir}]}" ]]; then
+          new_path+="${dir}:"
+          path_map[${dir}]=1
+      fi
+  done
+
+  # Remove trailing colon and export new PATH
+  export PATH="${new_path%:}"
+}
+
+# 1, collect paths
+# 2. add to path or skip
+# 3. reorder path
 
 
 # Mac OS paths
 if [[ "$(uname -s)" == "Darwin" ]]; then
   if ! command -v brew > /dev/null 2>&1; then
+    echo "Checking for Homebrew installation..."
     if [[ -x "/opt/homebrew/bin/brew" ]]; then
       brew_cmd="/opt/homebrew/bin/brew"
     elif [[ -x "/usr/local/bin/brew" ]]; then
@@ -38,21 +63,16 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     brew_cmd="brew"
   fi
 
-  mac_paths=(
-    "$(${brew_cmd} --prefix)/opt/gnu-getopt/bin"
-    "$(${brew_cmd} --prefix)/opt/coreutils/libexec/gnubin"
-    "$(${brew_cmd} --prefix)/bin"
-    "$(${brew_cmd} --prefix)/sbin"
-  )
-
-  for lpath in "${mac_paths[@]}"; do
-    setPath "${lpath}"
-  done
+  setPath "$(${brew_cmd} --prefix)/opt/gnu-getopt/bin"
+  setPath "$(${brew_cmd} --prefix)/opt/coreutils/libexec/gnubin"
+  setPath "$(${brew_cmd} --prefix)/sbin"
+  setPath "$(${brew_cmd} --prefix)/bin"
 
   unset brew_cmd
-  unset mac_paths
 fi
 
+# UNIX paths
+setPath "/usr/local/bin"
 
 # Local host paths
 if [[ -n "${local_paths}" ]]; then
---
+++ .bash_profile.d/99-1-post-path.sh
@@ -0,0 +1,9 @@
+# Post path
+
+# Reorder PATH to put custom paths at the front
+reorderPath
+
+# unset variables and functions
+unset -f CUSTOM_PATH
+unset -f setPath
+unset -f reorderPath
