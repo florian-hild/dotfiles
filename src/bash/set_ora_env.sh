e6e66ce7a3b6b3acd641ef03623f970a58560039
Florian Hild <florian.hild@sup-logistik.de>
2025-08-13 15:07:12 +0200

    feat(set_ora_env.sh): add tns_admin option

--- src/bash/set_ora_env.sh
+++ src/bash/set_ora_env.sh
@@ -25,6 +25,7 @@ Options:
   --home <PATH>                  Set path to Oracle home (required)
   --ldpath <PATH>                Set LD_LIBRARY_PATH (Default: \$ORACLE_HOME/lib)
   -s, --sid <ORACLE_SID>         Set Oracle SID
+  --tns_admin <PATH>             Set TNS_ADMIN (Default: \$ORACLE_HOME/network/admin)
   -v, --verbose                  Print debugging messages
   -V, --version                  Display version and exit
 
@@ -44,7 +45,7 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     help 128
   fi
 
-  OPTS="$(getopt -o "c:hs:vV" --long "characterset:,help,home:,ldpath:,sid:,verbose,version" -n "${progname}" -- "${@}")"
+  OPTS="$(getopt -o "c:hs:vV" --long "characterset:,help,home:,ldpath:,sid:,tns_admin:,verbose,version" -n "${progname}" -- "${@}")"
   if [[ "${?}" != "0" ]] ; then
     echo "Syntax or usage error (2)" >&2
     echo
@@ -71,6 +72,10 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
       ;;
     -s | --sid)
       declare -r oracle_sid="${2}"
+      shift 2
+      ;;
+    --tns_admin)
+      declare -r tns_admin="${2}"
       shift 2
       ;;
     -v | --verbose)
@@ -102,11 +107,9 @@ if [[ -z "${oracle_home// }" ]]; then
 fi
 
 if [[ -d "${oracle_home// }" ]]; then
-  echo "export ORACLE_HOME="${oracle_home// }""
-  echo "export ORACLE_HOME_LISTNER="${oracle_home// }""
-  # export TNS_ADMIN="${oracle_home// }/network/admin"
-  echo "export SQLPATH="${oracle_home// }/rdbms/admin:${SQLPATH}""
-  echo "export PATH="${oracle_home// }/bin:${oracle_home// }/OPatch:${PATH}""
+  echo "export ORACLE_HOME=\"${oracle_home// }\""
+  echo "export SQLPATH=\"${oracle_home// }/rdbms/admin:${SQLPATH}\""
+  echo "export PATH=\"${oracle_home// }/bin:${oracle_home// }/OPatch:${PATH}\""
 else
   echo "Error: Directory \"${oracle_home// }\" not found"
   exit 1
@@ -125,9 +128,9 @@ fi
 echo "export NLS_DATE_FORMAT='DD-MON-YYYY HH24:MI:SS'"
 
 if [[ -z "${ld_lib_path// }" ]]; then
-  echo "export LD_LIBRARY_PATH="${oracle_home}:${oracle_home}/lib:${LD_LIBRARY_PATH}""
+  echo "export LD_LIBRARY_PATH=\"${oracle_home}:${oracle_home}/lib:${LD_LIBRARY_PATH}\""
 elif [[ -d "${ld_lib_path// }" ]]; then
-  echo "export LD_LIBRARY_PATH="${ld_lib_path}:${LD_LIBRARY_PATH}""
+  echo "export LD_LIBRARY_PATH=\"${ld_lib_path}:${LD_LIBRARY_PATH}\""
 else
   echo "Error: ld_lib_path \"${ld_lib_path// }\" not found."
   echo
@@ -135,9 +138,18 @@ else
 fi
 
 if [[ -n "${oracle_sid// }" ]]; then
-  echo "export ORACLE_SID="${oracle_sid^^}""
+  echo "export ORACLE_SID=\"${oracle_sid^^}\""
 fi
 
+if [[ -z "${tns_admin// }" ]]; then
+  echo "export TNS_ADMIN=\"${oracle_home}/network/admin\""
+elif [[ -d "${tns_admin// }" ]]; then
+  echo "export TNS_ADMIN=\"${tns_admin}\""
+else
+  echo "Error: tns_admin \"${tns_admin// }\" not found."
+  echo
+  help 1
+fi
 
 
 
