58a443915060916939331114f5e3443ead646abd
Florian Hild <florian.hild@archbook.local>
2025-11-23 01:23:20 +0100

    delete: do not set locales

--- .bash_profile.d/00-1-locale.sh
+++
@@ -1,8 +0,0 @@
-# Mit diesen Einstellungen gibt es Probleme wenn der DSP mit C (ANSI) gestartet wird es aber eine UTF8 DB ist oder andersrum
-LANG="en_US.UTF-8"
-LC_COLLATE="en_US.UTF-8"
-LC_CTYPE="en_US.UTF-8"
-LC_MESSAGES="en_US.UTF-8"
-LC_MONETARY="de_DE.UTF-8"
-LC_NUMERIC="de_DE.UTF-8"
-LC_TIME="de_DE.UTF-8"
