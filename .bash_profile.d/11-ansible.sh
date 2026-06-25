adbb00d1780726537c36b84ed3a8e9686b266865
Florian Hild <florian.hild@sup-logistik.de>
2026-06-25 09:33:20 +0200

    refactor(ansible): simplify environment setup and remove containerized approach
    - Replace $ANSIBLE with $ANSIBLE_BASE for cleaner configuration
    - Remove container-based execution modes (Docker, venv, uv wrappers)
    - Delete Docker infrastructure and version-specific Dockerfiles
    - Consolidate to single latest Ansible setup structure
    - Simplify PATH and environment variable management

--- .bash_profile.d/11-ansible.sh
+++ .bash_profile.d/11-ansible.sh
@@ -1,88 +1,33 @@
 # shellcheck disable=SC2148
 # Ansible
 
-if [[ -n "${ANSIBLE// }" ]]; then
-  export ANSIBLE_HOME="${HOME}/.ansible"
-  export ANSIBLE_CONFIG="${ANSIBLE}/ansible.cfg"
-  export ANSIBLE_FORCE_COLOR=True
-  export INVENTORIES="${ANSIBLE}/environments"
-  export CUST="${INVENTORIES}/customers"
-  export INT="${INVENTORIES}/intern"
-  export TEST="${INVENTORIES}/test"
-  export PLAYBOOKS="${ANSIBLE}/playbooks"
-  export ROLES="${ANSIBLE}/roles"
-  export ROLESWIP="${ANSIBLE}/roles_wip"
+if [[ -n "${ANSIBLE_BASE// }" ]]; then
+    # Core / version
+    export ANSIBLE_VERSION="${ANSIBLE_VERSION:-"latest"}"
+    export ANSIBLE_ENV="${ANSIBLE_ENV:-"latest"}"
+    export ANSIBLE_HOME="${ANSIBLE_HOME:-"${ANSIBLE_BASE}/.config/${ANSIBLE_VERSION}"}"
+    export ANSIBLE_VENV="${ANSIBLE_HOME}/venv"
 
-  if [[ -n "${ANSIBLE_RUN_ENV// }" ]]; then
-    if [[ "${ANSIBLE_RUN_ENV// }" == "container" ]]; then
-      function ansible-container(){
-        local ansible_args="$*"
-        docker run \
-        --rm \
-        --init \
-        --volume ~/.ssh/"${ANSIBLE_DEFAULT_SSH_KEY-"id_ed25519"}":/home/ansible/.ssh/"${ANSIBLE_DEFAULT_SSH_KEY-"id_ed25519"}":ro \
-        --volume "${ANSIBLE}":/home/ansible/ansible:ro \
-        --volume "${ANSIBLE_HOME}":/home/ansible/.ansible:rw \
-        --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
-        --env WORK_PATH="/home/ansible/ansible/${PWD#*ansible/}" \
-        --name ansible-worker-"$(date +'%Y%m%d_%H%M%S')" \
-        local/ansible:"${ANSIBLE_DEFAULT_VERSION-"latest"}"-alpine \
-        bash -c "${ansible_args}"
-      }
+    # Ansible configuration
+    export ANSIBLE_CACHE_PLUGIN_CONNECTION="${ANSIBLE_BASE}/fact_cache"
+    export ANSIBLE_COLLECTIONS_PATHS="${ANSIBLE_HOME}/collections"
+    export ANSIBLE_CONFIG="${ANSIBLE_BASE}/ansible.cfg"
+    export ANSIBLE_HOST_KEY_CHECKING="${ANSIBLE_HOST_KEY_CHECKING:-"True"}"
+    export ANSIBLE_ROLES_PATH="${ANSIBLE_BASE}/roles_wip:${ANSIBLE_BASE}/roles"
+    export ANSIBLE_VAULT_PASSWORD_FILE="${ANSIBLE_BASE}/.vault_pass.sh"
 
-      function ansible-playbook(){
-        local ansible_playbook_args="$*"
-        ansible-container "ansible-playbook \${WORK_PATH}/${ansible_playbook_args}"
-      }
+    # Inventories
+    export INVENTORIES="${ANSIBLE_BASE}/environments"
+    export CUST="${INVENTORIES}/customers"
+    export INT="${INVENTORIES}/intern"
+    export TEST="${INVENTORIES}/test"
+    export ANSIBLE_INVENTORY="${ANSIBLE_INVENTORY:-"${TEST}"}"
 
-      function ansible-lint(){
-        local lint_target=${1:-"/home/ansible/ansible/${PWD#*ansible/}/*"}
-        local work_path_suffix=${PWD#*ansible/}
+    # Playbooks and roles
+    export PLAYBOOKS="${ANSIBLE_BASE}/playbooks"
+    export ROLES="${ANSIBLE_BASE}/roles"
+    export ROLESWIP="${ANSIBLE_BASE}/roles_wip"
 
-        echo "Lint target: ${lint_target}"
-        echo "Work path suffix: ${work_path_suffix}"
-        echo "Linting using yamllint..."
-        docker run \
-        --rm \
-        --init \
-        --volume "${ANSIBLE}":/home/ansible/ansible:ro \
-        --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
-        --env LINT_CONFIG_FILE="/home/ansible/ansible/${work_path_suffix%%/*}/.yamllint" \
-        --name ansible-worker-"$(date +'%Y%m%d_%H%M%S')" \
-        local/ansible:"${ANSIBLE_DEFAULT_VERSION:-"latest"}"-alpine \
-        bash -c "find ${lint_target} -type f \( -name '*.yaml' -o -name '*.yml' \) -exec yamllint -c \${LINT_CONFIG_FILE} {} +"
-
-        echo "Linting using ansible-lint..."
-        docker run \
-        --rm \
-        --init \
-        --volume "${ANSIBLE}":/home/ansible/ansible:ro \
-        --env VAULT_PASSWORD="${VAULT_PASSWORD}" \
-        --env LINT_CONFIG_FILE="/home/ansible/ansible/${work_path_suffix%%/*}/.ansible-lint" \
-        --name ansible-worker-"$(date +'%Y%m%d_%H%M%S')" \
-        local/ansible:"${ANSIBLE_DEFAULT_VERSION:-"latest"}"-alpine \
-        bash -c "ansible-lint --format brief --config-file \${LINT_CONFIG_FILE} ${lint_target}"
-      }
-    elif [[ "${ANSIBLE_RUN_ENV// }" == "venv" ]]; then
-      if [[ -n "${ANSIBLE_RUN_VENV_PATH// }" ]]; then
-        # shellcheck disable=SC2139
-        alias setansible="source ${ANSIBLE_RUN_VENV_PATH// }/bin/activate"
-      else
-        echo "Error: variable \$ANSIBLE_RUN_VENV_PATH not set."
-      fi
-    elif [[ "${ANSIBLE_RUN_ENV// }" == "uv" ]]; then
-        if [[ -n "${ANSIBLE_UV_PROJECT_PATH// }" ]]; then
-            function ansible { uv run --project "${ANSIBLE_UV_PROJECT_PATH// }" ansible "${@}"; }
-            function ansible-playbook { uv run --project "${ANSIBLE_UV_PROJECT_PATH// }" ansible-playbook "${@}"; }
-            function ansible-vault { uv run --project "${ANSIBLE_UV_PROJECT_PATH// }" ansible-vault "${@}"; }
-            function ansible-lint { uv run --project "${ANSIBLE_UV_PROJECT_PATH// }" ansible-lint "${@}"; }
-        else
-            echo "Error: variable \$ANSIBLE_UV_PROJECT_PATH not set."
-        fi
-    else
-    echo "Error: variable \$ANSIBLE_RUN_ENV has an unknown value: ${ANSIBLE_RUN_ENV// }"
-    fi
-  else
-    echo "Error: variable \$ANSIBLE_RUN_ENV not set."
-  fi
+    # PATH
+    export PATH="${ANSIBLE_VENV}/bin:${PATH}"
 fi
